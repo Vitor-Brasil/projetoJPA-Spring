2dfc63c3dfcbb48121ea79fadead7436db84fa6e
Vitor Brasil <vitor.brasil95@hotmail.com>
2023-08-23 11:02:11 -0300

    Service layer, component registration

--- src/main/java/com/example/projetojpa/repositories/UserRepository.java
+++ src/main/java/com/example/projetojpa/repositories/UserRepository.java
@@ -1,8 +1,10 @@
 package com.example.projetojpa.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.example.projetojpa.entities.User;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 }
