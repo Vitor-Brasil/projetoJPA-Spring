088ccec5de3434f4fd22f0193cf43aaf33ba48fb
Vitor Brasil <vitor.brasil95@hotmail.com>
2023-08-23 13:50:09 -0300

    Order, Instant, ISO 8601

--- src/main/java/com/example/projetojpa/entities/User.java
+++ src/main/java/com/example/projetojpa/entities/User.java
@@ -1,11 +1,16 @@
 package com.example.projetojpa.entities;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -20,6 +25,10 @@ public class User implements Serializable{
 	private String email;
 	private String phone;
 	private String password;
+	
+	@JsonIgnore// poderia colocar na classe order, dai no postman qdo puxasse /users/id, apareceria todos os pedidos de cada usuario
+	@OneToMany(mappedBy = "client")
+	private List<Order> orders = new ArrayList<>();
 	
 	public User() {
 	}
@@ -59,6 +68,9 @@ public class User implements Serializable{
 	}
 	public void setPassword(String password) {
 		this.password = password;
+	}
+	public List<Order> getOrders() {
+		return orders;
 	}
 	@Override
 	public int hashCode() {
